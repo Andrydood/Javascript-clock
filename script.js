dfba819c75f619efd91e7645691151a0c26f2f01
Andrea Casino <andrea.casino.2@gmail.com>
2017-01-17 01:11:39 +0000

    removed visual glitch when hands would complete circle

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
+var oldSeconds = 0;
+var oldMinutes = 0;
+var oldHours = 0;
+
+
 function setTime(){
   const date = new Date();
   const seconds = ((date.getSeconds()/60)*360)-90;
   const minutes = ((date.getMinutes()/60)*360)-90;
   const hours = ((date.getHours()/24)*360)-90;
 
+  //Define s,m,h as degrees in a circle
+
   const hourHand = document.querySelector('#hours')
   const minuteHand = document.querySelector('#minutes')
   const secondHand = document.querySelector('#seconds')
 
-  hourHand.style.transform=`rotate(${hours}deg)`;
-  minuteHand.style.transform=`rotate(${minutes}deg)`;
-  secondHand.style.transform=`rotate(${seconds}deg)`;
+  //Find dom elements
+
+  const hands = document.querySelectorAll('.hand');
+
+  if(seconds-oldSeconds<0){
+    //If about to loop, avoid glitch
+    hands.forEach(hand => hand.style.transition = 'all 0.0s');
+
+    hourHand.style.transform=`rotate(${hours}deg)`;
+    minuteHand.style.transform=`rotate(${minutes}deg)`;
+    secondHand.style.transform=`rotate(${seconds}deg)`;
+
+    }
+    else{
+
+    hands.forEach(hand => hand.style.transition = 'all 0.2s');
+
+    hourHand.style.transform=`rotate(${hours}deg)`;
+    minuteHand.style.transform=`rotate(${minutes}deg)`;
+    secondHand.style.transform=`rotate(${seconds}deg)`;
+    }
+
+  oldSeconds = seconds;
+  oldMinutes = minutes;
+  oldHours = hours;
+
 
 }
 
